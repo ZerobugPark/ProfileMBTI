6c7dd8e879038b3568081bc525a650d2c3ba7179
young <zerobugpark@gmail.com>
2025-02-07 23:40:47 +0900

    [파일추가]
    - ColorList 추가

---
+++ ProfileMBTI/ColorList/ColorList.swift
@@ -0,0 +1,18 @@
+//
+//  ColorList.swift
+//  ProfileMBTI
+//
+//  Created by youngkyun park on 2/7/25.
+//
+
+import UIKit
+
+
+enum ColorList {
+    
+    
+    static let white = UIColor.white
+    static let trueButton = #colorLiteral(red: 0.0986553058, green: 0.5292056203, blue: 0.9605885148, alpha: 1)
+    static let falseButton = #colorLiteral(red: 0.5490196078, green: 0.5490196078, blue: 0.5490196078, alpha: 1)
+    
+}
