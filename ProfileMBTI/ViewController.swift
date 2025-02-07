0ae89eb025beda05ebd291d49aa551451f09497c
young <zerobugpark@gmail.com>
2025-02-07 22:00:21 +0900

    Initial Commit

---
+++ ProfileMBTI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileMBTI
+//
+//  Created by youngkyun park on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
