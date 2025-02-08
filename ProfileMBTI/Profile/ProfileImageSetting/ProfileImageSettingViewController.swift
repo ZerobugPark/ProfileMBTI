a2164e61021822db57992926d7a8fbfb57235f08
young <zerobugpark@gmail.com>
2025-02-08 23:16:32 +0900

    [파일추가] .

---
+++ ProfileMBTI/Profile/ProfileImageSetting/ProfileImageSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileImageSettingViewController.swift
+//  ProfileMBTI
+//
+//  Created by youngkyun park on 2/8/25.
+//
+
+import UIKit
+
+class ProfileImageSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProfileMBTI/Profile/ProfileImageSetting/ProfileImageSettingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileImageSettingViewModel.swift
+//  ProfileMBTI
+//
+//  Created by youngkyun park on 2/8/25.
+//
+
+import Foundation
