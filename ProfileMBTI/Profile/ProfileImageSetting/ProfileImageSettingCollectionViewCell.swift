a132a16e3dda540838a59034757afea0558b9fba
young <zerobugpark@gmail.com>
2025-02-08 23:30:34 +0900

    파일명 수정

---
+++ ProfileMBTI/Profile/ProfileImageSetting/ProfileImageSettingCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileImageSettingCollectionViewCell.swift
+//  ProfileMBTI
+//
+//  Created by youngkyun park on 2/8/25.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class ProfileImageSettingCollectionViewCell: BaseCollectionViewCell {
+
+    
+    
+ 
+}
